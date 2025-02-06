caedc4b01196d7e64b65f134786f1baca9d8534f
karunakunde <karunaautomationtester@gmail.com>
2025-02-07 01:52:36 +0530

    ITestListner implemented with LoginTest class

---
+++ TranningProgram/src/test/java/testlistener/LoginTest.java
@@ -0,0 +1,96 @@
+package testlistener;
+
+import static org.testng.Assert.expectThrows;
+
+import org.checkerframework.checker.units.qual.t;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+//@Listeners(testlistener.MyListener.class)
+public class LoginTest {
+
+	/* in hard assertion if testcase failed then further excution will not happen*/
+	
+	WebDriver driver;
+	String url ;
+
+	
+	@Parameters({"browser","url"})
+	@BeforeMethod
+	void initialization(String browser,String url) {
+		 this.url = url;
+		switch (browser) {
+		case "Chrome":  driver = new ChromeDriver();
+			
+			break;
+
+		case "Edge":    driver = new EdgeDriver();
+			break;
+		case "Firefox": driver = new FirefoxDriver();
+			break;
+		default:  	    System.out.println("Illegal value");
+			break;
+		}
+	}
+	
+	@Test
+	void checkWindowTitle()
+	{
+		driver.get(url);
+		String expectedTitle = "CURA Healthcare Service";
+		String actualtitle = driver.getTitle();
+		
+		System.out.println("Before assertion: Check window Title");
+		Assert.assertEquals(expectedTitle,actualtitle );
+		System.out.println("After assertion: Check window Title");
+
+		
+		
+	}
+	@Test(priority = 2)
+	void checkUrl() {
+		
+		driver.get(url);
+		String expectedUrl = "http://www.facebook.com";
+		String actualUrl = driver.getCurrentUrl();
+		
+		System.out.println("Before assertion: Check current url");
+		Assert.assertEquals(expectedUrl, actualUrl);
+		System.out.println("After assertion: Check current url");
+	}
+	
+	@Test(priority = 3)
+	void checkHeadingDisplayed()
+	{
+		driver.get(url);
+		boolean isDisplayed = driver.findElement(By.xpath("//h1[text()='CURA Healthcare Service']")).isDisplayed();
+		System.out.println("Before Assertion: check heading is displyed");
+		Assert.assertTrue(isDisplayed);
+		System.out.println("Aftert assertion: check heading displyed");
+	}
+	
+	@Test(priority = 4,dependsOnMethods = {"checkUrl"})
+	void checkMakeAppointemnt() {
+		driver.get(url);
+		driver.findElement(By.xpath("//a[text()='Make Appointment']")).click();
+		String expectedRedirectedURl = "https://katalon-demo-cura.herokuapp.com/profile.php#login";
+		String actualRedirectedURL = driver.getCurrentUrl();
+		
+		System.out.println("Before Assetion: Check Make Appointment");
+		Assert.assertEquals(expectedRedirectedURl, actualRedirectedURL);
+		System.out.println("After Assetion: Check Make Appointment");
+
+		
+		
+	}
+	
+
+}
--- TranningProgram/src/test/java/testlistener/MyListener.java
+++ TranningProgram/src/test/java/testlistener/MyListener.java
@@ -1,8 +1,52 @@
 package testlistener;
 
+import org.testng.ITestContext;
 import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.annotations.Listeners;
 
 public class MyListener implements ITestListener {
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		System.out.println("Test started:"+result.getName());
+		
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		System.out.println("Test PASS:"+result.getName());
+
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		// TODO Auto-generated method stub
+		System.out.println("Test FAIL: "+result.getName());
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		System.out.println("Test SKIPED : "+result.getName());
+		
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing Started: "+context.getName());
+		
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing FINISHED :"+context.getName());
+		
+	}
 
 	
 	
